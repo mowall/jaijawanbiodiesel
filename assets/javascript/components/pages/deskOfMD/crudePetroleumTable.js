31a14a61f6b514ffda2c86414d7ff3cdaeda6740
nitishm-webonise <nitish.mowall@weboniselab.com>
2017-03-12 13:11:37 +0530

    updated table in mdvision

--- assets/javascript/components/pages/deskOfMD/crudePetroleumTable.js
+++ assets/javascript/components/pages/deskOfMD/crudePetroleumTable.js
@@ -6,7 +6,7 @@ export default class CrudePetroleumTable extends React.Component {
             <div className="col-md-12">
                 <h2 className='defaultHeader'>Import Burden on Crude Petroleum</h2>
                 <br />
-                <table className="table table-condensed">
+                <table className="table table-bordered">
   					<thead>
   						<tr>
   							<th>Year</th>
@@ -17,17 +17,17 @@ export default class CrudePetroleumTable extends React.Component {
   					<tbody>
   						<tr>
   							<th scope='row'>2004-05</th>
-  							<th>2005-06</th>
-  							<th>2006-07</th>
+  							<th>1,01,963</th>
+  							<th>22.60</th>
   						</tr>
   						<tr>
-  							<th scope='row'>1,01,963</th>
+  							<th scope='row'>2005-06</th>
   							<th>1,50,557</th>
-  							<th>1,90,000</th>
+  							<th>34.10</th>
   						</tr>
   						<tr>
-  							<th scope='row'>22.60</th>
-  							<th>34.10</th>
+  							<th scope='row'>2006-07</th>
+  							<th>1,90,000</th>
   							<th>43.30</th>
   						</tr>
   					</tbody>
