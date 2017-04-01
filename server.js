06c1c7b8e96a3193356216d0798c6a0c80efad3a
nitishm-webonise <nitish.mowall@weboniselab.com>
2017-04-01 19:35:52 +0530

    resize the images to smaller and used compression middleware

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const express = require('express')
-
+const compression = require('compression')
 module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, 'index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
-
+    
+    app.use(compression())
     app.use('/public', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
